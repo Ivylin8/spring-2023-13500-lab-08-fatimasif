7d18a07dc1b46858854123a7cfcc45e074c6d4d6
Ivylin8 <104948596+Ivylin8@users.noreply.github.com>
2023-05-04 21:52:07 -0400

    Update funcs.cpp
    added what i suggested
    -ivy lin

--- funcs.cpp
+++ funcs.cpp
@@ -24,7 +24,7 @@ void invert(int img[MAX_H][MAX_W], int& h, int& w) {
     // inverts the colors of the right half of the image
 void invertRightHalf(int img[MAX_H][MAX_W], int& h, int& w) {
     for (int row = 0; row < h; row++) {
-        for (int col = 0; col < w; col++) {   
+        for (int col = w/2; col < w; col++) {   
             // modified the inner loop to only iterate over the columns on the right half of the image by 
             // setting the loop's starting point to w/2
             // which is the index of the first column in the right half of the image
